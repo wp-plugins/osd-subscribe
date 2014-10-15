753d6e3c47730d49208ea5dd4b8bcff1993aa802
osdwebdev <osdwebdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-15 12:54:39 +0000

    Added banners
    git-svn-id: https://plugins.svn.wordpress.org/osd-subscribe/tags/1.1@1007886 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,7 @@
 <?php
+// Prevent direct access to file
+defined('ABSPATH') or die("No script kiddies please!");
+
 // If uninstall not called from WordPress exit
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
